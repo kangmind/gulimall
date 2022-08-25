d80587ba921a0ee62e767ff27d058d123c56328f
kang <1035372898@qq.com>
2022-08-26 02:22:26 +0800

    初始化网关代码

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
@@ -3,6 +3,22 @@ package com.atguigu.gulimall.coupon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+/**
+ * 选用nacos作配置中心：
+ * 1.引入依赖
+ * <dependency>
+ * <groupId>com.alibaba.cloud</groupId>
+ * <artifactId>spring-cloud-starter-alibaba-nacos-config</artifactId>
+ * </dependency>
+ * 2.resource下创建bootstrap.properties
+ * spring.application.name=gulimall-coupon
+ * spring.cloud.nacos.config.server-addr=127.0.0.1:8848
+ * 3.在nacos中添加一个匹配值
+ * Data-Id 的默认位 应用名.properties
+ * 4.@value配合@RefreshScope(注释在bean上)获取配置 配置项以nacos为准
+ */
 
 @EnableDiscoveryClient
 @SpringBootApplication
